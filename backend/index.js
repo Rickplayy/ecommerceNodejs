8b4397dff75f2d20d4c820c14862253581b08a90
Rickplayy <cuernicornio123@outlook.com>
2026-08-18 01:08:17 -0600

    Allow Netlify origin in CORS

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,8 @@ console.log('--- Executing index.js (DEBUG) ---');
  const corsOptions = {
    origin: [
      'http://ecommerce-rockpa-frontend.s3-website.us-east-2.amazonaws.com',
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://spontaneous-brigadeiros-39d714.netlify.app'
    ],
    credentials: true,
    optionsSuccessStatus: 200
